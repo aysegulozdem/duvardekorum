c622aa32d70d1f94ec91c64483ab9709f8ddbb1a
aysegulozdem <aysegulozdem78@gmail.com>
2025-07-25 20:39:34 +0300

    yenile

---
+++ products.js
@@ -0,0 +1,108 @@
+[
+  {
+    "id": 1,
+    "title": "Pembe Düşler Posteri",
+    "price": 100,
+    "description": "Doğadan sana gelen pembe düşler var.",
+    "image": "image/1.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Horizon",
+    "price": 100,
+    "description": "Uzayın derinlikleri sana da ilham olacak.",
+    "image": "image/2.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Dipsiz denizler",
+    "price": 250,
+    "description": "Modern tasarımlı DenizAltı posteri.",
+    "image": "image/3.jpg"
+  },
+  {
+    "id": 4,
+    "title": "Lavanta Bahçesi",
+    "price": 150,
+    "description": "Bahçe,balkon tasarımlarınızı süsleyecek son dokunuş.",
+    "image": "image/4.jpg"
+  },
+  {
+    "id": 5,
+    "title": "Tatil posteri",
+    "price": 150,
+    "description": "Tatil manifesti için gerekli poster.",
+    "image": "image/5.jpg"
+  },
+  {
+    "id": 6,
+    "title": "Serinlik",
+    "price": 150,
+    "description": "Kafe dekorasyonu arayanlar için.",
+    "image": "image/6.jpg"
+  },
+  {
+    "id": 7,
+    "title": "Manzara Poster",
+    "price": 150,
+    "description": "Manzara severler buyrun.",
+    "image": "image/7.jpg"
+  },
+  {
+    "id": 8,
+    "title": "İhtişam",
+    "price": 150,
+    "description": "Dekorasyonda ihtişam arayanlar için.",
+    "image": "image/8.jpg"
+  },
+  {
+    "id": 9,
+    "title": "Ilık",
+    "price": 150,
+    "description": "Kitap okuma köşeleri için,Ilık hissiyatı veren poster.",
+    "image": "image/9.jpg"
+  },
+  {
+    "id": 10,
+    "title": "Umut",
+    "price": 150,
+    "description": "Bazen bir poster ilham olur yeni projene.",
+    "image": "image/10.jpg"
+  },
+  {
+    "id": 11,
+    "title": "Kuzey Işıkları",
+    "price": 150,
+    "description": "Elbet bir gün.",
+    "image": "image/11.jpg"
+  },
+  {
+    "id": 12,
+    "title": "Rotasız Kaptan",
+    "price": 150,
+    "description": "Giden gelmesin sen tatile çık.",
+    "image": "image/12.jpg"
+  },
+  {
+    "id": 13,
+    "title": "Nostalji",
+    "price": 150,
+    "description": "Tarih severler burada mı ?",
+    "image": "image/13.jpg"
+  },
+  {
+    "id": 14,
+    "title": "Şefkat",
+    "price": 150,
+    "description": "Bir güneş parlar bazen.",
+    "image": "image/14.jpg"
+  },
+  {
+    "id": 15,
+    "title": "Cesaret",
+    "price": 150,
+    "description": "Adrenalin seninle olsun.",
+    "image": "image/15.jpg"
+  }
+ 
+]
